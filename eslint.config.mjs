cfa70bc2fe21c6cf8803dfc66a009f492e9b3d27
Alessio Frittoli <info@alessiofrittoli.it>
2025-12-04 18:25:18 +0100

    bump: update project deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,13 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
-/**
- * Initial file generated with `npm lint -- --init`
- * 
- * @type {import('eslint').Linter.Config[]}
- */
-// @ts-expect-error `languageOptions` property in `tseslint.configs.recommended` result incompatible with `eslint` config types.
-const tseslintReccommended = tseslint.configs.recommended
+// Initial file generated with `npm lint -- --init`
 
 /** @type {import('eslint').Linter.Config[]} */
 const config = [
 	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 	pluginJs.configs.recommended,
-	...tseslintReccommended,
+	...tseslint.configs.recommended,
 	{ files: [ 'src/**/*.{js,mjs,cjs,ts}' ] },
 	{ ignores: [ 'dist', 'scripts', 'coverage' ] },
 	{ rules: {
